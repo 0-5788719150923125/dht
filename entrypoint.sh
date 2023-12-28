7ae5cf2eefe003494d66c5ab5443fbce1c74867a
Luciferian Ink <LuciferianInk@protonmail.com>
2023-12-27 19:04:42 -0600

    make identities deterministic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ hivemind-dht \
     --use_ipfs \
     --use_auto_relay \
     --refresh_period 30 \
-    --host_maddrs \
-    "/ip4/0.0.0.0/tcp/4002,/ip4/0.0.0.0/udp/4002" \
-    # --announce_maddrs \
-    # "/ip4/0.0.0.0/tcp/4002,/ip4/0.0.0.0/udp/4002" \
     --identity_path /data/identity.key \
-    
+    --host_maddrs \
+    "/ip4/0.0.0.0/tcp/4002,/ip4/0.0.0.0/udp/4002"
